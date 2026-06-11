df91c4b41be86b26bb224dc301a26d3783c6d970
Devanshtiwari0806 <devansht4522@gmail.com>
2026-06-11 16:01:18 +0530

    Day_15 Questions

---
+++ Day_15/Question57.cpp
@@ -0,0 +1,37 @@
+// Program to reverse an array
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n;
+
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) 
+    {
+        cin >> arr[i];
+    }
+
+    int start = 0, end = n - 1;
+    while (start < end) 
+    {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        start++;
+        end--;
+    }
+    cout << "Reversed array is: ";
+    for (int i = 0; i < n; i++) 
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
---
+++ Day_15/Question60.cpp
@@ -0,0 +1,44 @@
+// Program to move zeroes in an array to the end 
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n;
+
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++) 
+    {
+        cin >> arr[i];
+    }
+
+    int index = 0;
+
+    for (int i = 0; i < n; i++) 
+    {
+        if (arr[i] != 0) 
+        {
+            arr[index] = arr[i];
+            index++;
+        }
+    }
+
+    while (index < n) 
+    {
+        arr[index] = 0;
+        index++;
+    }
+
+    cout << "Array after moving zeroes to the end : ";
+    for (int i = 0; i < n; i++) 
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
