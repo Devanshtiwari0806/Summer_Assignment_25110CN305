08991ec8a6f2e234bd12130d818ddc1ee3ef867d
Devanshtiwari0806 <devansht4522@gmail.com>
2026-06-17 19:01:45 +0530

    Day_21 Questions

---
+++ Day_21/Question84.cpp
@@ -0,0 +1,25 @@
+// Program to convert Lowercase to Uppercase
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char str[100];
+    int i = 0;
+
+    cout << "Enter a string: ";
+    cin.getline(str, 100);
+
+    while (str[i] != '\0')
+    {
+        if (str[i] >= 'a' && str[i] <= 'z')
+        {
+            str[i] = str[i] - 32;
+        }
+
+        i++;
+    }
+    cout << "String in uppercase = " << str;
+    return 0;
+}
